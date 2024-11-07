d0bcbf7e48f21bb1d70647ffb727fe7fa310b5f4
Nick <144923650+Jamahdd@users.noreply.github.com>
2024-11-07 18:40:26 +0000

    jsPopQuiz

---
+++ unit3_javascript/jsFoundation.js
@@ -0,0 +1,9 @@
+// Data types = buldings blocks of the Javascript
+// Programming language.
+
+// Strings = data bound together by qoutation marks
+"this is a sentence wrapped in qoutes- this is a string"
+"1232321- this is a number But, when in qoutes its a string"
+
+// Integer = numbers
+// our a decimal
